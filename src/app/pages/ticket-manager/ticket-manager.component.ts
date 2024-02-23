c0f82cd87678fd247af7870eace0d75c42fcd509
luiz-rissardi <rissardi.luiz2006@gmail.com>
2024-02-23 17:54:45 -0300

    teste de acesso a camera

--- src/app/pages/ticket-manager/ticket-manager.component.ts
+++ src/app/pages/ticket-manager/ticket-manager.component.ts
@@ -8,3 +8,20 @@ import { Component } from '@angular/core';
 export class TicketManagerComponent {
 
 }
+document.addEventListener("DOMContentLoaded", function() {
+  const cameraView = document.getElementById('cameraView');
+
+  // Função para acessar a câmera e exibir o vídeo
+  function startCamera() {
+      navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+          .then(function(stream) {
+              (cameraView as HTMLVideoElement).srcObject = stream;
+          })
+          .catch(function(err) {
+              console.error('Erro ao acessar a câmera: ', err);
+          });
+  }
+
+  // Chamando a função para iniciar a câmera quando o DOM estiver carregado
+  startCamera();
+});
